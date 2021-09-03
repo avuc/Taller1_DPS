68f5e347dc554356617c0412b8f8beba4377319d
Francisco Valle <vallecesco@gmail.com>
2021-09-03 00:00:10 -0600

    Un buen cambio

--- ropa-accesorios/src/components/Form.jsx
+++ ropa-accesorios/src/components/Form.jsx
@@ -4,15 +4,33 @@ import Autocomplete from "@material-ui/lab/Autocomplete";
 
 const Form = () => {
   let ropas = require("../json/ropa.json");
-  console.log(ropas[0].nombre);
+  const [nombre, setNombre] = useState(ropas[0].nombre);
+  const [precio, setPrecio] = useState(ropas[0].precio);
+
+  /*console.log(ropas[0].nombre);*/
   const nombres = ropas.map((ropa) => ropa.nombre);
+
+  const handleChange = (e) => {
+    console.log(nombre + "---" + precio);
+  };
 
   return (
     <>
+    <div>{console.log(nombre + "---" + precio)}</div>
+    <div>{console.log(nombre)}</div>
       <form onSubmit={(e) => e.preventDefault()}>
         <br />
         <Autocomplete
-          options={nombres}
+          options={ropas}
+          getOptionLabel={(option) => option.nombre}
+          nombre={nombre}
+          onChange={(event, newNombre) => {
+            setNombre(newNombre);
+          }}
+          precio={precio}
+          onInputChange={(event, newPrecio) => {
+            setPrecio(newPrecio);
+          }}
           style={{ width: 300 }}
           renderInput={(params) => (
             <TextField {...params} label="Ropas" variant="outlined" />
